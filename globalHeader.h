0ed35794dfaf9072042647e1810e617bae17e93a
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2020-06-21 18:52:40 +0430

    Create globalHeader.h

---
+++ globalHeader.h
@@ -0,0 +1,28 @@
+//string color
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+//module on/off
+#define ON 1
+#define OFF 0
+
+//input adress
+char logAddress[240];
+char judgeFileAdress[240];
+char inOutFileAdress[240];
+
+
+//module state
+int judgeModuleState;
+int generatorModuleState;
+int testCaseViewState;
+
+
+
+
