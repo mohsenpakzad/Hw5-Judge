d403ef960d672b4697fca79b551bb5b18fecaeba
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2020-06-23 23:09:06 +0430

    Update globalHeader.h

--- globalHeader.h
+++ globalHeader.h
@@ -1,3 +1,4 @@
+#define ADRESS_ARRAY_SIZE 512
 //string color
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
@@ -11,11 +12,14 @@
 //module on/off
 #define ON 1
 #define OFF 0
-
+//fucntion answer
+#define YES 1
+#define NO 0
 //input adress
-char logAddress[240];
-char judgeFileAdress[240];
-char inOutFileAdress[240];
+char logAddress[ADRESS_ARRAY_SIZE];
+char judgeFileAdress[ADRESS_ARRAY_SIZE];
+char inOutFileAdress[ADRESS_ARRAY_SIZE];
+char suspectFileAdress[ADRESS_ARRAY_SIZE];
 
 
 //module state
@@ -23,6 +27,9 @@ int judgeModuleState;
 int generatorModuleState;
 int testCaseViewState;
 
-
-
+//function
+int dirExist(const char fileAdress[ADRESS_ARRAY_SIZE]);
+int typeCExist(const char fileAdress[ADRESS_ARRAY_SIZE]); 
+int compile(char fileAdress[ADRESS_ARRAY_SIZE]); //notDone
+int ioFileCounter(const char fileAdress[ADRESS_ARRAY_SIZE]);
 
