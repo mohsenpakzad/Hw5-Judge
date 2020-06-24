cffba8cbf7dc12ddbd9740d5cee9f042267c71ab
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2020-06-24 13:09:44 +0430

    Update generatorHeader.h

--- generator/generatorHeader.h
+++ generator/generatorHeader.h
@@ -1,29 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+#include "globalHeader.h"
 #define MAX_TEST_CASE 1000
 
 #pragma once
 
 int numberOfTestCases;
 
-void moveToAnAddress(char address[]){
-   char str[100+3];
-  strcpy(str, "cd ");
-  strcpy(str, address);
-  //str = "cd address"
-  system(str);
-}
+void moveToAnAddress(char address[]);
+
   
-void giveInputToCodeAndSaveOutput(char codeAddress[],char inputAddress[],char outputAddress[]){
-  char linuxCode[300+6];
-  strcpy(linuxCode, codeAddress);
-  strcat(linuxCode, " < ");
-  strcat(linuxCode, inputAddress);
-  strcat(linuxCode, " > ");
-  strcat(linuxCode, outputAddress);
-  //linuxCode = "codeAddress < inputAddress > outputAddress"
-  system(linuxCode); 
-}
+void giveInputToCodeAndSaveOutput(char codeAddress[],char inputAddress[],char outputAddress[]);
+int judgeCompile(const char fileAdress[ADRESS_ARRAY_SIZE]);
+
   
