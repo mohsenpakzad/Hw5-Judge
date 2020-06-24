16ecfb72da209ac7576b0dfc41594332ea858596
kasrax33 <67009450+kasrax33@users.noreply.github.com>
2020-06-24 12:18:52 +0430

    Update generatorHeader.h

--- generator/generatorHeader.h
+++ generator/generatorHeader.h
@@ -1,3 +1,24 @@
 #pragma once
-
 // declare your functions and global variables here
+
+int numberOfTestCases;
+
+void linuxCodeToMoveToAnAddress(char address[]){
+   char str[100+3];
+  strcpy(str, "cd ");
+  strcpy(str, address);
+  //str = "cd address"
+  system(str);
+}
+  
+void linuxCodeToGiveInputToCodeAndSaveOutput(char codeAddress[],char inputAddress[],char outputAddress[]){
+  char linuxCode[300+6];
+  strcpy(linuxCode, codeAddress);
+  strcat(linuxCode, " < ");
+  strcat(linuxCode, inputAddress);
+  strcat(linuxCode, " > ");
+  strcat(linuxCode, outputAddress);
+  //linuxCodeForOutputGeneration = "codeAddress < inputAddress > outputAddress"
+  system(linuxCode); 
+}
+  
