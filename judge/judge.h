05b75806ac9a40fcde542ef819bdefd721886b14
Amir Shamsi <59437623+EmirShamsi@users.noreply.github.com>
2020-06-24 13:45:29 +0430

    Update judge.h

--- judge/judge.h
+++ judge/judge.h
@@ -1,3 +1,12 @@
 #pragma once
 
-// declare your functions and global variables here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_LENGTH 1000
+#define ACCEPTED 0
+#define DECLINED 1
+ 
+#define MAX_OUTPUT_LENGTH 10000
