080f7be83376964f13151d7fec0fa1cb0d58f8a2
ramtinof <ramtin.ghassemi.nejad@gmail.com>
2020-07-02 03:59:13 +0430

    Revert "System protection added"
    This reverts commit ba5b9ed285cd55e1c6249ec0158c2b1711c78bfe.

--- common/changeColor/changeColor.c
+++ common/changeColor/changeColor.c
@@ -24,10 +24,6 @@ int changeColor (char color[]){
 		SetConsoleTextAttribute(hConsole, FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE);
 		return 1;
 	}
-	else if (strcmp(color,"yellow")==0||strcmp(color,"y")==0){
-		SetConsoleTextAttribute(hConsole, FOREGROUND_RED|FOREGROUND_GREEN);
-		return 1;
-	}
 	else {
 		return -1;
 	}
