f8197bda43f45922e63ea6a1db99c8b11a2f38a3
haniyenmati <66249416+haniyenmati@users.noreply.github.com>
2020-07-02 12:51:18 +0430

    Update judge.h

--- judge/judge.h
+++ judge/judge.h
@@ -1,3 +1,18 @@
 #pragma once
 
-// declare your functions and global variables here
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include <time.h>
+
+extern char codeName[];
+extern char fileName[];
+extern char codeAddress[];
+extern char fileAddress[];
+int timeLimit;
+
+
+int compareFile(FILE *, FILE *);
+
+void judgeProcces();
+
