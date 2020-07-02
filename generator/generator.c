2de3593b31256c92a470784cb6b7381fbdb492a9
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2020-07-02 14:01:41 +0430

    Update generator.c

--- generator/generator.c
+++ generator/generator.c
@@ -1,37 +1,40 @@
-//compile function
-//dont know what to add to the generatotHeader.c/.h
-//debug
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "generatorHeader.h"
 
-Compile()
-  
-int generator(char inOutAdress[],char codeAddress[]){
-  moveToAnAddress(inOutaddress);
-  
-  int i=1;
-  while(i<=numberOfTestCases){  //numberOfTestCases is defined in generatorHeader.h and is given value in main.c
-    char strNum[MAX_TEST_CASE]; //MAX_TEST_CASE is defined in generatorHeader.h
+int generator(const char fileAdress[ADRESS_ARRAY_SIZE])
+{
+  //moveToAnAddress(inOutAdress);
+  if (judgeCompile(fileAdress) == NO)
+    return NO;
+  int i = 1, numberOfTestCases = inFileCounter("TestCase/Input", ".in");
+  if (numberOfTestCases == -1)
+  {
+    printf("  \n%s\n\n", YEL "WARNING:" RESET "  generation process has malfunctioned,\n          Or one of the IO folders has been croupted.\n          You can try running " MAG "Generator" RESET " module again");
+    return -1;
+  }
+
+  while (i <= numberOfTestCases)
+  {
+    char strNum[ADRESS_ARRAY_SIZE / 4];
     sprintf(strNum, "%d", i); //int i into string
-    
+
     //create input name => "input\\in(number).in"
-    char inputName[11];
-    strcpy(inputName,"input\\in");
-    strcat(inputName, strNum); //add number to input name
-    strcat(inputName, ".in"); //add .in to input name
-    
-    //create output name => "output\\out(number).in"
-    char outputName[12];
-    strcpy(outputName,"output\\out");
+    char inputName[20];
+    strcpy(inputName, "TestCase/Input/");
+    strcat(inputName, strNum);
+    strcat(inputName, ".in");
+
+    //create output name => "output/out(number).in"
+    char outputName[20];
+    strcpy(outputName, "TestCase/Output/");
     strcat(outputName, strNum); //add number to output name
     strcat(outputName, ".out"); //add .out to output name
-    
-    giveInputToCodeAndSaveOutput(codeAddress, inputName, outputName);
-    
+
+    giveInputToCodeAndSaveOutput("/usr/local/adjudicator/temp/gavel", inputName, outputName);
+
     i++;
   }
-  return 1;
+  return YES;
 }
