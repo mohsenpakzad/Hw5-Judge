23d858a928ba9d114305ca94aad79551974ad51b
reza00farjam <67030594+reza00farjam@users.noreply.github.com>
2020-07-02 17:43:13 +0430

    Update generator.h

--- generator/generator.h
+++ generator/generator.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_PATH_SIZE 100
+#define MAX_COMMAND_SIZE 1000
 
 int doesExist(const char* name);
 int generatorFromFile();
