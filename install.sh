ca73c3adbc19c3b804528d3d56bebf75a094ab61
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2020-07-02 18:59:09 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,14 @@ rm -f /usr/local/bin/adjudicator
 
 gcc -c main.c globalHeader.c generator/generator.c generator/generatorHeader.c judge/judgeHeader.c judge/judge.c #2> INSTALL_LOG.txt
 gcc -o adjudicator main.o globalHeader.o generator.o generatorHeader.o judgeHeader.o judge.o -lm 2> INSTALL_LOG.txt #UNFINISHED 
-     #1- compilation still needs changes
-     #2- gcc will get more arguments as development moves forward #adju is working program name as in adjudicator
-     #3- "2> INSTALL_LOG.txt" writes compilation log in .txt
 
- #set to $PATH
- #export PATH=$PATH:/place/with/the/file #this has some security issues -but for now will be ignored-
+     #1- "2> INSTALL_LOG.txt" writes compilation log in .txt
 
-#ALTERNATIVE add to usr/bi
 
+sudo apt install rename
 sudo cp adjudicator /usr/local/bin
 sudo rm -rf adjudicator
 sudo rm -rf /usr/local/adjudicator
 sudo mkdir /usr/local/adjudicator
 sudo mkdir /usr/local/adjudicator/temp
-#sudo cp -R Script /usr/local/adjudicator
 rm *.o
