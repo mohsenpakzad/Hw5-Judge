575e6b7d590f4a620692e9ca0c5c3f85e92c2b38
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2020-07-02 14:19:02 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ sudo apt-get install build-essential #making sure host system has gcc and all es
 rm -f /usr/local/bin/adjudicator
      #COMPILE PROGRESS
 
-gcc -c HW2.4.c globalHeader.c generator.c generatorHeader.c judgeHeader.c Judge.c #2> INSTALL_LOG.txt
-gcc -o adjudicator HW2.4.o globalHeader.o generator.o generatorHeader.o judgeHeader.o Judge.o -lm 2> INSTALL_LOG.txt #UNFINISHED 
+gcc -c main.c globalHeader.c generator/generator.c generator/generatorHeader.c judge/judgeHeader.c judge/Judge.c #2> INSTALL_LOG.txt
+gcc -o adjudicator main.o globalHeader.o generator.o generatorHeader.o judgeHeader.o Judge.o -lm 2> INSTALL_LOG.txt #UNFINISHED 
      #1- compilation still needs changes
      #2- gcc will get more arguments as development moves forward #adju is working program name as in adjudicator
      #3- "2> INSTALL_LOG.txt" writes compilation log in .txt
@@ -16,11 +16,9 @@ gcc -o adjudicator HW2.4.o globalHeader.o generator.o generatorHeader.o judgeHea
 #ALTERNATIVE add to usr/bi
 
 sudo cp adjudicator /usr/local/bin
+sudo rm -rf adjudicator
 sudo rm -rf /usr/local/adjudicator
 sudo mkdir /usr/local/adjudicator
 sudo mkdir /usr/local/adjudicator/temp
 #sudo cp -R Script /usr/local/adjudicator
 rm *.o
-#NOTES For unistall
-#1- remove from bin
-#2- remove from local
