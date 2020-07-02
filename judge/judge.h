b6a1b23a8ab241d2e683ee0ea708181290b54611
ShotgunkeeperVZ <55543338+ShotgunkeeperVZ@users.noreply.github.com>
2020-07-02 14:29:26 +0430

    Update judge.h

--- judge/judge.h
+++ judge/judge.h
@@ -4,9 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define MAX_LENGTH 1000
-#define ACCEPTED 0
-#define DECLINED 1
- 
 #define MAX_OUTPUT_LENGTH 10000
+int suspectCompile(char fileAdress[ADRESS_ARRAY_SIZE]);
