dc4b806fe11aecdf325555efa0882d32628f70d8
reza00farjam <67030594+reza00farjam@users.noreply.github.com>
2020-07-02 14:29:55 +0430

    Update generator.h

--- generator/generator.h
+++ generator/generator.h
@@ -1,9 +1,6 @@
 #pragma once
 
-char input[10000000000];
-
-char exefile[1000];
-
-int doesExist(const char *name);
-
-void testCases();
+int doesExist(const char* name);
+int generatorFromFile();
+int generatorFromCMD();
+int generator();
